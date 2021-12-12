ae019475fa20d1934cd564736a73436668b719ed
Louis <accounts@louislouw.me>
2021-12-12 17:16:28 +0200

    compress stats

--- src/routes/repl/compress.svelte
+++ src/routes/repl/compress.svelte
@@ -4,9 +4,12 @@
 	import LZString from 'lz-string';
 	import { storageAllowed, storageMock, StorageNotice } from 'svelte-repl-storagemock';
 
-	const storageKey = 'compressedChainUser';
-	let isREPL = true;
+	const storageKey = 'compressedChainPoem';
+	let replEnv = true;
 	let user;
+	let uncompressedSize = 0;
+	let compressedSize = 0;
+	let saving = '0.0';
 
 	onMount(() => {
 		/**
@@ -16,31 +19,46 @@
 		 * 3. window.localStorage only available at mount, so we can only create during this lifecycle phase.
 		 */
 
-		isREPL = !storageAllowed();
-		const storage = isREPL ? storageMock() : window.localStorage;
+		replEnv = !storageAllowed(); //If storage is not allowed, we assume we are running on REPL
+		const storage = replEnv ? storageMock() : window.localStorage;
 		user = chain(jsonChainLink())
+			.chain((v) => {
+				uncompressedSize = new Blob([v]).size;
+				return v;
+			})
 			.chain(LZString.compress, LZString.decompress)
 			.chain(storageChainLink(storageKey, storage))
+			.chain((v) => {
+				compressedSize = new Blob([v]).size;
+				return v;
+			})
 			.store({
-				name: 'John',
-				age: 18
+				author: 'Edgar Allan',
+				poem: `The Bells by Edgar Allan
+To the swinging and the ringing
+of the bells, bells, bells
+Of the bells, bells, bells, bells
+Bells, bells, bells
+To the rhyming and the chiming of the bells!`
 			});
 	});
+
+	$: saving = (((uncompressedSize - compressedSize) / uncompressedSize) * 100).toFixed(1);
 </script>
 
-<StorageNotice storageAllowed={!!isREPL} />
+<StorageNotice show={replEnv} />
 
 {#if user}
-	<h1>Please enter user details:</h1>
+	<h1>Edit Poem</h1>
 	<div>
-		Name:<br />
-		<input bind:value={$user.name} placeholder="Name" />
+		Author:<br />
+		<input bind:value={$user.author} placeholder="Author" />
 	</div>
 	<div>
-		Age:<br />
-		<input bind:value={$user.age} type="number" min="0" max="150" placeholder="Age" />
+		<textarea bind:value={$user.poem} rows="8" cols="60" />
 	</div>
 	<div>
-		Change the details and then refresh. It should load the page with your saved user details.
+		Uncompressed size is {uncompressedSize} and the compressed is {compressedSize}, giving a
+		<b>{saving}%</b> saving.
 	</div>
 {/if}
