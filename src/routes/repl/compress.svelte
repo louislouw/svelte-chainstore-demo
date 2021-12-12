398a86acc5bd35036b72805846351524880154ee
Louis <accounts@louislouw.me>
2021-12-12 16:28:13 +0200

    compress using new repl storage mock package

--- src/routes/repl/compress.svelte
+++ src/routes/repl/compress.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { chain, jsonChainLink, storageChainLink } from 'svelte-chainstore';
 	import LZString from 'lz-string';
+	import { storageAllowed, storageMock, StorageNotice } from 'svelte-repl-storagemock';
 
 	const storageKey = 'compressedChainUser';
 	let isREPL = true;
@@ -10,61 +11,24 @@
 	onMount(() => {
 		/**
 		 * Note:
-		 * 1. This demo will NOT work on REPL as access to localStorage is blocked for security reasons.
+		 * 1. This demo will NOT PERSIST data on REPL as access to localStorage is blocked for security reasons.
 		 * 2. storageChainLink requires values to be string, so we need to use jsonChainLink to convert to/from string.
 		 * 3. window.localStorage only available at mount, so we can only create during this lifecycle phase.
 		 */
 
+		isREPL = !storageAllowed();
+		const storage = isREPL ? storageMock() : window.localStorage;
 		user = chain(jsonChainLink())
 			.chain(LZString.compress, LZString.decompress)
-			.chain(storageChainLink(storageKey, replStorageCheck()))
+			.chain(storageChainLink(storageKey, storage))
 			.store({
 				name: 'John',
 				age: 18
 			});
 	});
-
-	function replStorageCheck() {
-		let storage;
-		//Due to security we are not allowed to use storage on REPL
-		//Thus we use a dummy storage mockup
-		//Note: This storage mockup will unfortunately not persist data between browser refreshes
-		try {
-			storage = window.localStorage;
-			isREPL = false;
-		} catch (e) {
-			storage = storageMock();
-		}
-		return storage;
-	}
-
-	const storageMock = () => {
-		let internalStore = {};
-		return {
-			getItem: (key) => internalStore[key],
-			setItem: (key, value) => {
-				internalStore[key] = value;
-			},
-			clear: () => {
-				internalStore = {};
-			},
-			key: (index) => Object.keys(internalStore)[index],
-			removeItem: (key) => {
-				internalStore[key] = null;
-			},
-			length: Object.keys(internalStore).length
-		};
-	};
 </script>
 
-{#if isREPL}
-	<h1>REPL NOTICE</h1>
-	<p>
-		This example will unfortunately not perist data if run on REPL.<br />
-		REPL blocks access to window.localStorage due to security concerns.<br />
-		Thus you will have to run code locally to see it in action.
-	</p>
-{/if}
+<StorageNotice storageAllowed={!!isREPL} />
 
 {#if user}
 	<h1>Please enter user details:</h1>
